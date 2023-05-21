4c626422660ee2e9ea8f08c21d2a72454028d522
Ivanka077 <122215710+Ivanka077@users.noreply.github.com>
2023-05-21 23:28:48 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
 
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const emailEl = form.elements.email.value;
+    const passwordEl = form.elements.password.value;
+
+    if (emailEl === '' || passwordEl === '') {
+        alert('Заповніть всі поля, будь ласка');
+    } else {
+        const dataInfo = {
+            email: emailEl,
+            password: passwordEl,
+        }; 
+        console.log(dataInfo);
+        form.reset();
+    }
+}
