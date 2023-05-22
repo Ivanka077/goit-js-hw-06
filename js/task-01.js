99cbb3e0705a7da7dc43a65e855d6b6d8a857c5b
Ivanka077 <122215710+Ivanka077@users.noreply.github.com>
2023-05-22 15:58:39 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,10 +2,15 @@ const idEl = document.getElementById('categories');
 const itemEl = idEl.querySelectorAll('li.item');
 console.log(`Number of categories: ${idEl.children.length}`);
 
+itemEl.forEach((item) => {
+    const categoryTitle = item.firstElementChild.textContent;
+    const categoryEl = item.lastElementChild.children.length;
+
+    console.log(`Category: ${categoryTitle}`);
+    console.log(`Elements: ${categoryEl}`);
+
+});
 
 
-
-// const titleEl = idEl.firstChild;
-// console.log(titleEl);
 
 
