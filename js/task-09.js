a9d4c221b20e471e5f71026609c216bab359286e
Ivanka077 <122215710+Ivanka077@users.noreply.github.com>
2023-05-22 03:03:52 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,22 @@
+
+const refs = {
+  btnEl: document.querySelector('.change-color'),
+  bodyEl: document.body,
+  spanEl: document.querySelector('.color'),
+};
+
+
+refs.btnEl.addEventListener('click', changeBodyColor);
+
+function changeBodyColor() {
+  const randomColor = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = randomColor;
+  refs.spanEl.textContent = randomColor;
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
