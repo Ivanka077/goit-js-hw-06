e472eee62aec4d1afd359886ac9e5c80baf7e62a
Ivanka077 <122215710+Ivanka077@users.noreply.github.com>
2023-05-22 15:56:43 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,8 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const ulEl = document.querySelector('.gallery');
+const markup = images.map((image) => `<li class='items'><img class='item-img' src="${image.url}" alt="${image.alt}"/></li>`).join('');
+
+ulEl.insertAdjacentHTML('beforeend', markup);
